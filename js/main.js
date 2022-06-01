25e3d8644271b9c5cd92a2309eabd350cc960074
Mrq9rawi <moh.abdullah02@gmail.com>
2022-05-31 22:37:04 -0400

    added an alert and added my framework css file

--- js/main.js
+++ js/main.js
@@ -6,6 +6,36 @@ let convertedParagraph = document.getElementById("paragraph");
 
 let requestData = new XMLHttpRequest();
 
+function showAlert() {
+	let alertDiv = document.createElement("div");
+	let alertOverlay = document.createElement("div");
+	let alertContent = document.createElement("div");
+	let alertCrossMark = document.createElement("div");
+	let alertP = document.createElement("p");
+
+	alertDiv.id = "alert";
+	alertOverlay.classList.add("overlay");
+	alertContent.classList.add("content");
+	alertCrossMark.classList.add("cross-mark");
+	alertP.classList.add("text");
+	alertP.textContent = "Please make sure to use 3 characters only for the first and second fields, and use Number only for the third filed";
+
+	alertOverlay.addEventListener("click", deleteAlert);
+	alertCrossMark.addEventListener("click", deleteAlert);
+
+	function deleteAlert() {
+		alertDiv.remove();
+	}
+
+	document.body.append(alertDiv);
+	alertDiv.append(alertOverlay);
+	alertDiv.append(alertContent);
+	alertContent.append(alertCrossMark);
+	alertContent.append(alertP);
+}
+
+showAlert();
+
 currencyForm.addEventListener("submit", (e) => {
 	e.preventDefault();
 
@@ -18,12 +48,14 @@ currencyForm.addEventListener("submit", (e) => {
 
 	if (numbersRe.test(amount) === true && charactersRe.test(toCurrency) === true && charactersRe.test(fromCurrency)) {
 		requestDataFromApi(fromCurrency, toCurrency, amount);
+	} else {
+		showAlert();
+
 	}
 });
 
 function requestDataFromApi(fromCurrency, toCurrency, amount) {
 	requestData.open("GET", `https://metals-api.com/api/convert?access_key=7kv2bceg306bgn7xf8h205aswjv413m4tj4z7ekbu8t8oyht8dfit0iwdb7x&%20from=${fromCurrency}&to=${toCurrency}&amount=${amount}`);
-	// https://metals-api.com/api/convert?access_key=7kv2bceg306bgn7xf8h205aswjv413m4tj4z7ekbu8t8oyht8dfit0iwdb7x&%20from=GBP&to=JPY&amount=25
 
 	requestData.onreadystatechange = function () {
 		if (requestData.readyState === 4 && requestData.status === 200) {
@@ -31,6 +63,7 @@ function requestDataFromApi(fromCurrency, toCurrency, amount) {
 			convertedParagraph.textContent = `${dataToUse.query.amount} ${dataToUse.query.from} = ${dataToUse.result.toFixed(2)} ${dataToUse.query.to}`;
 		}
 	};
+
 	requestData.send();
 }
 
